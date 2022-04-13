e46bc36f74ad1dafd12d4155e468b5f74e61570a
Juan Jimenez (JJ) <juan.jimenez@pobox.com>
2022-04-13 11:29:14 +0200

    Initial commit

---
+++ src/test/java/com/example/sfgrecipe/SfgRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sfgrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

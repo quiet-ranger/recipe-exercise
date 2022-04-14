994b10afb9d56d66506d4b42d75c107a84a647cf
Juan Jimenez (JJ) <juan.jimenez@pobox.com>
2022-04-14 20:03:41 +0200

    Created OneToMany and ManyToOne relationships

--- src/main/java/com/example/sfgrecipe/model/Recipe.java
+++ src/main/java/com/example/sfgrecipe/model/Recipe.java
@@ -1,6 +1,7 @@
 package com.example.sfgrecipe.model;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -18,6 +19,10 @@ public class Recipe {
     private String directions;
 
     // private Difficulty difficulty;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
+
     @Lob
     private Byte[] image;
 
@@ -87,6 +92,14 @@ public class Recipe {
 
     public void setDirections(String directions) {
         this.directions = directions;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 
     public Byte[] getImage() {

f255f5599dcaff88ae2afd2e778029dd4131fc4c
Juan Jimenez (JJ) <juan.jimenez@pobox.com>
2022-04-14 19:50:07 +0200

    Created two initial entities

--- src/main/java/com/example/sfgrecipe/SfgRecipeApplication.java
+++ src/main/java/com/example/sfgrecipe/SfgRecipeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SfgRecipeApplication {
 
     public static void main(String[] args) {
+        System.out.println("Starting up quicker than usual!");
         SpringApplication.run(SfgRecipeApplication.class, args);
     }
 
